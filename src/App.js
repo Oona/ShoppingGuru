929f4bf10e4334b49140c6293d69e69835c7584e
Oona Kangas <oona.kangass@gmail.com>
2018-08-01 10:32:54 +0300

    NavMenu toimii, App.js muutokset

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import logo from "./logo.svg";
 import "./App.css";
 import TuoteForm from "./Komponentit/TuoteForm";
 import NavMenu from "./Komponentit/NavMenu";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import lisaa from "./Komponentit/lisaa";
+import ostoslista from "./Komponentit/ostoslista";
+import kori from "./Komponentit/kori";
+
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <NavMenu />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <TuoteForm />
-        </p>
+ 
+        <Switch>
+            <Route exact path="/Oona" component={lisaa} />
+            <Route exact path="/ostosl" component={ostoslista} />
+            <Route exact path="/kori" component={kori} />
+           
+          </Switch>
+  
       </div>
+      </Router>
     );
   }
 }
--- src/Komponentit/NavMenu.js
+++ src/Komponentit/NavMenu.js
@@ -1,27 +1,19 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import lisaa from "./lisaa";
-import ostoslista from "./ostoslista";
-import kori from "./kori";
-import NotFound from "./NotFound";
+
 
 class NavMenu extends Component {
   render() {
     return (
-      <Router>
+
         <div>
-          <Link to="/">Lisää tuotteita</Link>
+          <Link to="/Oona">Lisää tuotteita</Link>
           <Link to="/ostosl">Ostoslista</Link>
           <Link to="/kori">Kori</Link>
           <Link to="/eiole">Error</Link>
-          <switch>
-            <Route exact path="/" component={lisaa} />
-            <Route exact path="/ostosl" component={ostoslista} />
-            <Route exact path="/kori" component={kori} />
-            <Route exact path="/eiole" component={NotFound} />
-          </switch>
+         
         </div>
-      </Router>
+
     );
   }
 }
