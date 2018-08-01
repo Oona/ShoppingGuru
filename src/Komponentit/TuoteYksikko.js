741034c0acbb68b84fc8dd6c2d08434a4a427ee8
tatuvahteri <tatu.vahteri@gmail.com>
2018-08-01 16:36:52 +0300

    tuotu muutokset JO branchistä, korjattu mergekonfliktit

--- src/Komponentit/TuoteYksikko.js
+++ src/Komponentit/TuoteYksikko.js
@@ -5,25 +5,13 @@ import './TuoteYksikko.css';
 
 class Tuoteyksikko extends Component {
 
-<<<<<<< HEAD
-render(){
-    let todoItems;
-    console.log(this.props.todos);
-    if(this.props.todos){
-        todoItems=this.props.todos.map(todo => {
-            return(
-                <Tuote todo ={todo} poista={this.props.poisto}  key={todo.tuotteennimi} />
-            );
-        }
-    );
-=======
     render() {
         let todoItems;
         console.log(this.props.todos);
         if (this.props.todos) {
             todoItems = this.props.todos.map(todo => {
                 return (
-                    <li className="Lista"><Tuote key={todo.tuotteennimi} todo={todo} /></li>
+                    <Tuote todo ={todo} poista={this.props.poisto}  key={todo.tuotteennimi} />
                 );
             });
         }
@@ -36,7 +24,6 @@ render(){
 
             </div>
         );
->>>>>>> c32e86baee6f4d163e380e627dc77369bf25279d
     }
 }
 
