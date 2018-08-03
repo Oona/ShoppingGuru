910f4572d73cc21b8280a49a9a92fcfc98a0680a
tatuvahteri <tatu.vahteri@gmail.com>
2018-08-03 11:19:36 +0300

    herjat poistettu käännöstä

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./Komponentit/tyyli.css";
 import NavMenu from "./Komponentit/NavMenu";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import TuoteList from "./Komponentit/TuoteList";
-import TuoteForm from "./Komponentit/TuoteForm";
 import KoriList from "./Komponentit/KoriList";
 
 class App extends Component {
